d21035e58cba951d1b4b42435d5c3c7ceda3d6c6
corazonjs <corazondev@mail.ru>
2024-03-18 12:50:14 +0500

    Like functionality

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,15 +1,30 @@
 import "./Post.css";
-import {MoreVert, Favorite} from "@mui/icons-material";
-import { Users } from "../../DynamicData"
+import { MoreVert, FavoriteBorder, Favorite } from "@mui/icons-material"; // импортируем иконку Favorite
+import { Users } from "../../DynamicData";
+import { useState } from "react";
 
-export default function Post({post}) {
+export default function Post({ post }) {
+    const [like, setLike] = useState(post.like);
+    const [isLiked, setIsLiked] = useState(false);
+
+    const likeHandler = () => {
+        setLike(isLiked ? like - 1 : like + 1);
+        setIsLiked(!isLiked);
+    };
+
     return (
         <div className="post">
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img className="postProfileImg" src={Users.filter(u=> u.id === post.userId)[0].profilePicture} alt=""/>
-                        <span className="postUsername">{Users.filter(u=> u.id === post.userId)[0].username}</span>
+                        <img
+                            className="postProfileImg"
+                            src={Users.filter((u) => u.id === post.userId)[0].profilePicture}
+                            alt=""
+                        />
+                        <span className="postUsername">
+                            {Users.filter((u) => u.id === post.userId)[0].username}
+                        </span>
                         <span className="postDate">{post.date}</span>
                     </div>
                     <div className="postTopRight">
@@ -19,12 +34,15 @@ export default function Post({post}) {
                 <div className="postCenter">
                     <span className="postText">{post?.desc}</span>
                     <img className="postImg" src={post.photo} alt="" />
-
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
-                        <Favorite className="likeIcon"/>
-                        <span className="postLikeCounter">{post.like} people like it</span>
+                        {isLiked ? (
+                            <Favorite onClick={likeHandler} className="likeIcon" />
+                        ) : (
+                            <FavoriteBorder onClick={likeHandler} className="likeIcon" />
+                        )}
+                        <span className="postLikeCounter">{like} people like it</span>
                     </div>
                     <div className="postBottomRight">
                         <span className="postCommentText">{post.comment} comments</span>
